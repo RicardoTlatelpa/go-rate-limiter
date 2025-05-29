bbe44ba6c0e508854956b35aa0b59d3409f0b32a
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2025-05-28 21:37:13 -0400

    created NewTokenBucket function and Allow() function along with TokenBucket struct with imported mutex API and time API

--- limiter/token_bucket.go
+++ limiter/token_bucket.go
@@ -22,6 +22,41 @@ type TokenBucket struct {
 	capacity int
 	tokens float64
 	refillRate float64
-	lastRefillRate time.Time
+	lastRefillTime time.Time
 	mu sync.Mutex // thread safety
+}
+
+func NewTokenBucket(capacity int, refillRate float64) *TokenBucket {
+	return &TokenBucket{
+		capacity: capacity,
+		tokens: float64(capacity),
+		refillRate: refillRate,
+		lastRefillTime: time.Now(),
+	}
+}
+
+func (tb *TokenBucket) Allow() bool {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+	
+	now := time.Now()
+	elapsed := now.Sub(tb.lastRefillTime).Seconds()
+	refilledTokens := elapsed * tb.refillRate
+
+	tb.tokens = min(float64(tb.capacity), tb.tokens+refilledTokens)
+	tb.lastRefillTime = now
+
+	if tb.tokens >= 1 {
+		tb.tokens -= 1
+		return true
+	}
+
+	return false
+}
+
+func min(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
 }
